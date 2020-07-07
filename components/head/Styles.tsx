819925100c3a8afb8cdeca848944b05324923969
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-07 14:51:46 -0400

    inline links and styles

---
+++ components/head/Styles.tsx
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default () => (
+  <Head>
+
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;500;600;800;900&display=swap" rel="stylesheet"/>
+        
+  </Head>
+);
