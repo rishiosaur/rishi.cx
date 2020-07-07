272f8b19c44bdb111629fa8a4f65a739899299d4
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-07 16:07:17 -0400

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Inter', 'sans-serif']
+      },
+      screens: {
+        'dark': {'raw': '(prefers-color-scheme: dark)'},
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
