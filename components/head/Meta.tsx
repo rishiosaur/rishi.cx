954467f06aa8d962813320d448f5c67ac38b967b
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-07 14:51:40 -0400

    meta component

---
+++ components/head/Meta.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+export default () => (
+  <Head>
+    <meta name="title" content="Rishi Kothari — Developer" />
+    <meta
+      name="description"
+      content="Hi! I'm Rishi, a rising high school sophomore developer in Canada that's passionate about impacting billions."
+    />
+    <meta
+      name="keywords"
+      content="math, entrepreneurship,rishi,kothari,developer,dev,"
+    />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <meta name="robots" content="index, follow" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="English" />
+    <meta name="revisit-after" content="3 days" />
+    <meta name="author" content="Rishi Kothari" />
+  </Head>
+);
